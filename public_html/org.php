914343c79aff4eb5c00d162a76353e7ae6261b2e
Akshay Naik <akshaynaik404@gmail.com>
2017-02-08 18:54:10 +0530

    fix #1 show auth access tab to admin only

--- public_html/org.php
+++ public_html/org.php
@@ -36,7 +36,19 @@
             <a class="mdl-navigation__link compose-btn">Compose</a>
             <a class="link-sent mdl-navigation__link ">Sent</a>
             <a class="link-groups mdl-navigation__link ">Groups</a>
-            <a class="link-authorized mdl-navigation__link ">Authorized Users</a>
+            <!-- <a class="link-authorized mdl-navigation__link ">Authorized Users</a> -->
+             <?php
+             $root_dir = $_SERVER['DOCUMENT_ROOT'];
+             if (isset($_COOKIE['org_notifier_id'])) {
+                 include $root_dir.'server/database_functions/org_profile.php';
+                 $org_notifier_id = $_COOKIE['org_notifier_id'];
+                 session_start();
+                 $notifier_id = $_SESSION['notifier_id'];
+                 if (is_admin_access($org_notifier_id, $notifier_id)) {
+                     echo '  <a class="link-authorized mdl-navigation__link ">Authorized Users</a>';
+                 }
+             }
+              ?>
             <a class="logout-btn mdl-navigation__link">Logout</a>
           </nav>
         </div>
