2eba8f5f9f9d95a3e520349a1c3351a6bcc246e8
Akshay Naik <akshaynaik404@gmail.com>
2017-02-23 02:07:59 +0530

    change otp validation method from email to mobile number.

--- src/index/app/forgot-password.js
+++ src/index/app/forgot-password.js
@@ -1,37 +1,43 @@
 // import $ from 'jQuery';
 
-import {vars} from './vars.js';
+import {
+	vars
+} from './vars.js';
 
 // forgot  password submit SUCCESS handler
-vars.$forgotPasswordContainer.find('.forgot-password-submit-btn').on('click', function () {
-  var email = vars.$forgotPasswordContainer.find("#email").val();
-  let $progressBar = vars.$forgotPasswordContainer.find('.mdl-progress');
-  let $clickedBtn = this;
-  // console.log(email);
+vars.$forgotPasswordContainer.find('.forgot-password-submit-btn').on('click',
+	function () {
+		var mobile_no = vars.$forgotPasswordContainer.find("#mobile_no").val();
+		let $progressBar = vars.$forgotPasswordContainer.find('.mdl-progress');
+		let $clickedBtn = this;
+		// console.log(mobile_no);
 
-  // show loading state
-  $progressBar.show();
-  $clickedBtn.disabled = true;
+		// show loading state
+		$progressBar.show();
+		$clickedBtn.disabled = true;
 
-  $.ajax({
-    type: "POST",
-    url: "/server/forgot_password/forgot_password.php",
-    data: {
-      email: email
-    }
-  }).done(function (data) {
-    // show request is complete
-    $progressBar.hide();
-    $clickedBtn.disabled = false;
-    // console.log(data);
-    if ($.trim(data) === "1") {
-      vars.$recoverySuccess.fadeIn();
-    } else if($.trim(data) === "0"){
-      vars.$forgotPasswordContainer.find('.mdl-textfield').addClass('is-invalid');
-    } else {
-      console.log("server error: ", data);
-    }
-  }).fail(function (event, jqxhr, settings, thrownError) {
-    console.log(event + jqxhr.status + settings + thrownError);
-  });
-});
+		$.ajax({
+			type: "POST",
+			url: "/server/forgot_password/forgot_password.php",
+			data: {
+				mobile_no: mobile_no
+			}
+		}).done(function (data) {
+			// show request is complete
+			$progressBar.hide();
+			$clickedBtn.disabled = false;
+			// console.log(data);
+			if ($.trim(data) === "1") {
+				vars.$recoverySuccess.fadeIn();
+				alert('Otp sent. Check mobile phone');
+				location.href = './forgot_password_otp_validation.html';
+			} else if ($.trim(data) === "0") {
+				vars.$forgotPasswordContainer.find('.mdl-textfield').addClass(
+					'is-invalid');
+			} else {
+				console.log("server error: ", data);
+			}
+		}).fail(function (event, jqxhr, settings, thrownError) {
+			console.log(event + jqxhr.status + settings + thrownError);
+		});
+	});
--- src/index/app/signup.js
+++ src/index/app/signup.js
@@ -1,45 +1,55 @@
 // import $ from 'jQuery';
 
-import {vars} from './vars.js';
-import {functions} from './functions';
+import {
+	vars
+} from './vars.js';
+import {
+	functions
+} from './functions';
 $(function () {
 	// let vars.$personalSignupContainer = $(".personal-signup-container");
-	vars.$personalSignupContainer.find(".personal-signup-submit-btn").click(function (e) {
-		e.preventDefault();
-		let inputIds = ["#email", "#new-notifier-id", "#new-password", "#confirm-password"];
-		signUp(inputIds, vars.$personalSignupContainer, "personal_temp_signup", this);
-	});
+	vars.$personalSignupContainer.find(".personal-signup-submit-btn").click(
+		function (e) {
+			e.preventDefault();
+			let inputIds = ["#mobile_no", "#new-notifier-id", "#new-password",
+				"#confirm-password"];
+			signUp(inputIds, vars.$personalSignupContainer, "personal_temp_signup",
+				this);
+		});
 });
 
 function signUp(inputIds, containerSelector, dataEndpointUrl, $clickedBtn) {
-  // Instead use jquery serialize
-  let dataObj = (function(ids) {
-    dataObj = {};
-    ids.forEach(function(id) {
-      dataObj[id.slice(1, id.length)] = functions.getValue(id, containerSelector);
-    });
-    return dataObj;
-  })(inputIds);
+	// Instead use jquery serialize
+	let dataObj = (function (ids) {
+		dataObj = {};
+		ids.forEach(function (id) {
+			dataObj[id.slice(1, id.length)] = functions.getValue(id,
+				containerSelector);
+		});
+		return dataObj;
+	})(inputIds);
 	let $progressBar = vars.$personalSignupContainer.find('.mdl-progress');
 
 	// indicate signup loading
 	$progressBar.show();
 	$clickedBtn.disabled = true;
 
-  $.ajax({
-    type: "POST",
-    data: dataObj,
-    url: "/server/" + dataEndpointUrl + ".php"
-  }).done(function(data) {
+	$.ajax({
+		type: "POST",
+		data: dataObj,
+		url: "/server/" + dataEndpointUrl + ".php"
+	}).done(function (data) {
 
 		$progressBar.hide();
 		$clickedBtn.disabled = false;
 
-    if($.trim(data) === '1') {
-      window.alert('Verification link sent, Please check your e-mail inbox/spam.');
-    } else if($.trim(data) === '0') {
-      window.alert('Unable to process request');
-    } else {
+		if ($.trim(data) === '1') {
+			window.alert(
+				'Otp sent, Please check your mobile phone.');
+			location.href = './personal_otp_validation.html';
+		} else if ($.trim(data) === '0') {
+			window.alert('Unable to process request');
+		} else {
 			console.log(data);
 			try {
 				data = JSON.parse(data);
@@ -47,16 +57,16 @@ function signUp(inputIds, containerSelector, dataEndpointUrl, $clickedBtn) {
 				console.log(e);
 			}
 
-      for (var item in data) {
-        var $errorInput = $('#' + item);
-        var $errorInputContainer = $errorInput.parent();
-        //visually indicate error
-        $errorInputContainer.addClass('is-invalid');
-        //add error msg to error span
-        $errorInput.siblings('span').html(data[item]);
-      }
-    }
-  }).fail(function(event, jqxhr, settings, thrownError) {
-    console.log(event + jqxhr.status + settings + thrownError);
-  });
+			for (var item in data) {
+				var $errorInput = $('#' + item);
+				var $errorInputContainer = $errorInput.parent();
+				//visually indicate error
+				$errorInputContainer.addClass('is-invalid');
+				//add error msg to error span
+				$errorInput.siblings('span').html(data[item]);
+			}
+		}
+	}).fail(function (event, jqxhr, settings, thrownError) {
+		console.log(event + jqxhr.status + settings + thrownError);
+	});
 }
